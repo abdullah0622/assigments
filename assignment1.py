cb9d2b5182d32f91cb61e033cbc5dae890dc69f5
abdullah <primezonstr@gmail.com>
2020-04-23 19:08:56 -0500

    assignment1 added

---
+++ assignment1.py
@@ -0,0 +1,6 @@
+firstname = "Joseph"
+name = input("enter first name")
+if name == firstname:
+    print("Hello, Joseph! The password is : @12")
+else:
+    print(f"Hello, {name.capitalize()}! See you later.")
